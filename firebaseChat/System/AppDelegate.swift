8775b98a4af8ddb0240e51e886942009678de47c
PeaceKim <me@peacekim.com>
2020-01-18 13:28:09 +0900

    FirstProjectCommmit

---
+++ firebaseChat/System/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  firebaseChat
+//
+//  Created by Henry on 2020/01/18.
+//  Copyright © 2020 tpsinc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
