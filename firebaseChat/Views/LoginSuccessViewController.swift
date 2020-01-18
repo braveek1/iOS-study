834f22bb145cbc59aa58fccef0fdf7d40ba4a0b8
PeaceKim <me@peacekim.com>
2020-01-18 14:58:09 +0900

    Added Login Success View Controller

---
+++ firebaseChat/Views/LoginSuccessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginSuccessViewController.swift
+//  firebaseChat
+//
+//  Created by Henry on 2020/01/18.
+//  Copyright © 2020 tpsinc. All rights reserved.
+//
+
+import UIKit
+
+class LoginSuccessViewController: UIViewController {
+    
+}
